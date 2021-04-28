d3f122d499f2357e5e25b1e9df40447ab83a2fbc
YasuhiroYano <yasu@kkyes.co.jp>
2021-04-28 17:55:35 +0900

    グラフ表示を追加した

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+app_name = 'graph_app'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/',views.post_new,name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('graph/', views.graph, name='graph'),
+    path('chart/', views.get_svg, name="plot"),
+    path('scatter/', views.get_png, name='scatter'),
+ 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -39,3 +40,50 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+from django.http import HttpResponse
+import io
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# グラフ作成
+
+def setPlt():   
+    # 折れ線グラフを出力
+    # TODO: 本当はpkを基にしてモデルからデータを取得する。
+    x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12])
+    y = np.array([20, 90, 50, 30, 100, 80, 10, 60, 40, 70,35,20])
+    plt.plot(x, y)
+# svgへの変換
+def pltToSvg():
+    buf = io.BytesIO()
+    plt.savefig(buf, format='svg', bbox_inches='tight')
+    s = buf.getvalue()
+    buf.close()
+    return s
+
+def graph(request):   
+      return render(request, 'blog/graph.html')
+
+def get_svg(request):
+    setPlt()       # create the plot
+    svg = pltToSvg() # convert plot to SVG
+    plt.cla()        # clean up plt so it can be re-used
+    return HttpResponse(svg, content_type='image/svg+xml')
+
+def get_png(request):
+    wine=pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv',sep=';')
+    x=wine.alcohol
+    y=wine.pH
+    plt.scatter(x,y)
+    plt.xlabel('alchol')
+    plt.ylabel('pH')
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight')
+    png = buf.getvalue()
+    buf.close()
+    plt.cla()        # clean up plt so it can be re-used
+    return HttpResponse(png, content_type='image/png+xml')
